999ad33cbc298bc655a27225f7e754ebb879cade
KelemenOzseb <kelemenozseb@gmail.com>
2025-01-17 12:48:10 +0100

    DtoProvider added

--- WebStore.Entities/Dtos/Item/ItemShortViewDto.cs
+++ WebStore.Entities/Dtos/Item/ItemShortViewDto.cs
@@ -8,7 +8,9 @@ namespace WebStore.Entities.Dtos.Item
 {
     public class ItemShortViewDto
     {
+        public string Id { get; set; }
         public string StoreId { get; set; }
+        public string StoreName { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
---
+++ WebStore.Logic/Helper/DtoProvider.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Entities.Dtos.Item;
+using WebStore.Entities.Dtos.Rating;
+using WebStore.Entities.Entity_Models;
+
+namespace WebStore.Logic.Helper
+{
+    public class DtoProvider
+    {
+        public Mapper Mapper { get; }
+
+        public DtoProvider()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Item, ItemShortViewDto>()
+                .AfterMap((src, dest) =>
+                {
+                    dest.AverageRating = src.Ratings?.Count > 0 ? src.Ratings.Average(r => r.Rate) : 0;
+                });
+                cfg.CreateMap<Item, ItemViewDto>();
+                cfg.CreateMap<ItemCreateDto, Item>();
+                cfg.CreateMap<RatingCreateDto, Rating>();
+                cfg.CreateMap<Rating, RatingViewDto>();
+            });
+
+            Mapper = new Mapper(config);
+        }
+    }
+}
