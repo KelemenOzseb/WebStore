b04d14e950f597c0f0cc11427818bfd9a883629e
KelemenOzseb <kelemenozseb@gmail.com>
2025-01-17 16:42:31 +0100

    Login Done +installed nuget package

--- WebStore/Controllers/UserController.cs
+++ WebStore/Controllers/UserController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebStore.Entities.Dtos.User;
 
 namespace WebStore.Endpoint.Controllers
@@ -20,7 +24,7 @@ namespace WebStore.Endpoint.Controllers
             await userManager.CreateAsync(user, dto.Password);
         }
         [HttpPost("login")]
-        public async Task Login(UserInputDto dto)
+        public async Task<IActionResult> Login(LoginInputDto dto)
         {
             var user = await userManager.FindByNameAsync(dto.UserName);
             if (user == null)
@@ -36,10 +40,41 @@ namespace WebStore.Endpoint.Controllers
                 }
                 else
                 {
+                    //todo: generate token
+                    var claim = new List<Claim>();
+                    claim.Add(new Claim(ClaimTypes.Name, user.UserName!));
+                    claim.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+
+                    foreach (var role in await userManager.GetRolesAsync(user))
+                    {
+                        claim.Add(new Claim(ClaimTypes.Role, role));
+                    }
+
+                    int expiryInMinutes = 24 * 60;
+                    var token = GenerateAccessToken(claim, expiryInMinutes);
+
+                    return Ok(new LoginResultDto()
+                    {
+                        Token = new JwtSecurityTokenHandler().WriteToken(token),
+                        Expiration = DateTime.Now.AddMinutes(expiryInMinutes)
+                    });
 
                 }
             }
+        }
 
+        private JwtSecurityToken GenerateAccessToken(IEnumerable<Claim>? claims, int expiryInMinutes)
+        {
+            var signinKey = new SymmetricSecurityKey(
+                  Encoding.UTF8.GetBytes("NagyonhosszútitkosítókulcsNagyonhosszútitkosítókulcsNagyonhosszútitkosítókulcsNagyonhosszútitkosítókulcsNagyonhosszútitkosítókulcsNagyonhosszútitkosítókulcs"));
+
+            return new JwtSecurityToken(
+                  issuer: "WebStore.com",
+                  audience: "WebStore.com",
+                  claims: claims?.ToArray(),
+                  expires: DateTime.Now.AddMinutes(expiryInMinutes),
+                  signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                );
         }
     }
 }
