226a48542edcfa90ccd997a4b62a1da82dbfcf1c
KelemenOzseb <kelemenozseb@gmail.com>
2025-01-17 16:29:36 +0100

    register+login

---
+++ WebStore/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Entities.Dtos.User;
+
+namespace WebStore.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserManager<IdentityUser> userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpPost("register")]
+        public async Task Register(UserInputDto dto)
+        {
+            var user = new IdentityUser(dto.UserName);
+            await userManager.CreateAsync(user, dto.Password);
+        }
+        [HttpPost("login")]
+        public async Task Login(UserInputDto dto)
+        {
+            var user = await userManager.FindByNameAsync(dto.UserName);
+            if (user == null)
+            {
+                throw new ArgumentException("User not found");
+            }
+            else
+            {
+                var result = await userManager.CheckPasswordAsync(user, dto.Password);
+                if (!result)
+                {
+                    throw new ArgumentException("Incorrect password");
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}
