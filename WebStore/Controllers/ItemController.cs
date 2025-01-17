8d6a2f2167a0892b632db4fa749b4c6249130797
KelemenOzseb <kelemenozseb@gmail.com>
2025-01-17 13:15:25 +0100

    ItemController done

--- WebStore/Controllers/ItemController.cs
+++ WebStore/Controllers/ItemController.cs
@@ -1,6 +1,41 @@
-﻿namespace WebStore.Endpoint.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Entities.Dtos.Item;
+using WebStore.Logic.Logic;
+
+namespace WebStore.Endpoint.Controllers
 {
-    public class ItemController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
     {
+        ItemLogic logic;
+        public ItemController(ItemLogic logic)
+        {
+            this.logic = logic;
+        }
+        [HttpPost]
+        public void AddItems(ItemCreateDto dto)
+        {
+            logic.AddItem(dto);
+        }
+
+        [HttpGet]
+        public IEnumerable<ItemShortViewDto> GetAllItems()
+        {
+            return logic.GetAllItems();
+        }
+        [HttpPut("{id}")]
+        [Authorize]
+        public void UpdateItem(string id, [FromBody] ItemCreateDto dto)
+        {
+            logic.UpdateItem(id, dto);
+        }
+
+        [HttpGet("{id}")]
+        public ItemViewDto GetItem(string id)
+        {
+            return logic.GetItem(id);
+        }
     }
 }
